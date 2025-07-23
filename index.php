0bca2fd2d24f1437d389602df20de4c977d43fc7
Ronal David Valderrama Gomez <189985514+ronaldavidvalderrama@users.noreply.github.com>
2025-07-23 00:16:41 +0000

    feat: :sparkles: actualizacion de clase

--- index.php
+++ index.php
@@ -5,55 +5,45 @@ require_once "vendor/autoload.php";
 use App\Middleware\JsonBodyParserMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use psr\Http\Server\RequestHandlerInterface as Handler;
+use Psr\Http\Server\RequestHandlerInterface as Handler;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
-header('Content-Type: application/json');
-
-$app->get('/', function(Request $req, Response $res, array $args) {
-    $res->getBody()->write(json_encode(["message" => "hola desde Slim"]));
+$app->get('/', function (Request $req, Response $res, array $args) {
+    $res->getBody()->write(json_encode(["message" => "Hola desde Slim"]));
     return $res;
 });
+// Middlewares
+//Global -> a todas las Request del Backend
 
-//middleware
-//global > a todas las request del backend
-
-$app->add(function(Request $req, Handler $han): Response {
+$app->add(function (Request $req, Handler $han): Response {
     $response = $han->handle($req);
-    return $response->withHeader('Content-Type', 'application/json');   
+    return $response->withHeader('Content-Type', 'application/json');
 });
-
-// custom Global Middleware
+// Custom Global Middleware
 $app->add(new JsonBodyParserMiddleware());
 
 
+// GET    /campers
+// POST   /campers
+// PUT    /campers/1
+// PATH   /campers/1
+// DELETE /campers/1
 
-
-
-
-
-
-// GET    / campers
-// POST   / campers
-// PUT    / campers/1
-// PATCH  / campers/1
-// DELETE / campers/1
-
-$app->get("/campers/{name}/{skill}", function(Request $req, Response $res, array $args) {
-    // GET localhost:8081/campers/Adrian/php
-    $name = $args["name"] ?? "default";
-    $skill = $args["skill"] ?? "default";
+$app->get("/campers/{name}/{skill}", function (Request $req, Response $res, array $args) {
+    // GET    localhost:8081/campers/Adrian/php
+    $name = $args["name"];
+    $skill = $args["skill"];
     $res->getBody()->write(json_encode([$name, $skill]));
     return $res;
-})->add(function(Request $req, Handler $han): Response {
-    $reponse = $han->handle($req);
-    return $reponse->withHeader('x-powered-By', 'Slim framework');
+})->add(function (Request $req, Handler $han): Response {
+    $response = $han->handle($req);
+    return $response->withHeader('X-Powered-By', 'Slim Framework');
 });
 
-$app->get("/campers", function(Request $req, Response $res, array $args) {
-    // GET localhost:8081/campers?name=Adrian&php=php
+$app->get("/campers", function (Request $req, Response $res, array $args) {
+    // GET    localhost:8081/campers?skill=php
     $params = $req->getQueryParams();
     $name = $params["name"] ?? "default";
     $skill = $params["skill"] ?? "default";
@@ -61,13 +51,12 @@ $app->get("/campers", function(Request $req, Response $res, array $args) {
     return $res;
 });
 
-$app->post("/campers", function(Request $req, Response $res, array $args) {
+$app->post("/campers", function (Request $req, Response $res, array $args) {
     $data = $req->getParsedBody();
-    var_dump($data['name'] ?? 'Desconocido :(');
-    //$res->withStatus(201);
+    //$res = $res->withStatus(201);
     $res->getBody()->write(json_encode($data));
     return $res->withStatus(201);
+    //return $res;
 });
 
 $app->run();
-
--- src/Middleware/JsonBodyParserMiddleware.php
+++ src/Middleware/JsonBodyParserMiddleware.php
@@ -2,16 +2,15 @@
 
 namespace App\Middleware;
 
-use psr\Http\Message\ResponseInterface as Response;
-use psr\Http\Message\ServerRequestInterface as Request;
-use psr\Http\Server\MiddlewareInterface;
-use psr\Http\Server\RequestHandlerInterface as Handler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface as Handler;
 
-class JsonBodyParserMiddleware implements MiddlewareInterface 
+class JsonBodyParserMiddleware implements MiddlewareInterface
 {
-    public function process(Request $request, Handler $handler): Response 
+    public function process(Request $request, Handler $handler): Response
     {
-
         $contentType = $request->getHeaderLine('Content-Type');
         if (strtr($contentType, ["application/json"])) {
             $data = json_decode(file_get_contents('php://input'), true);
